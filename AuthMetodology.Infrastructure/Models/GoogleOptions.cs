b8d7e37b369f5aabf732d3eed43a0a4e7d2982df
BelugaChan <tuchnyy@list.ru>
2025-03-24 20:52:14 +0300

    Added new models

---
+++ AuthMetodology.Infrastructure/Models/GoogleOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthMetodology.Infrastructure.Models
+{
+    public class GoogleOptions
+    {
+        public string ClientId { get; set; } = string.Empty;
+    }
+}
