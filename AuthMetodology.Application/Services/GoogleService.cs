f48335864c753a7620a78b6cc62240b32cfbf785
BelugaChan <tuchnyy@list.ru>
2025-03-24 20:49:41 +0300

    Added new google service

---
+++ AuthMetodology.Application/Services/GoogleService.cs
@@ -0,0 +1,29 @@
+﻿using AuthMetodology.Application.DTO;
+using AuthMetodology.Application.Interfaces;
+using AuthMetodology.Infrastructure.Models;
+using Google.Apis.Auth;
+using Microsoft.Extensions.Options;
+
+namespace AuthMetodology.Application.Services
+{
+    public class GoogleService : IGoogleService
+    {
+        private readonly GoogleOptions options;
+        public GoogleService(IOptions<GoogleOptions> options)
+        {
+            this.options = options.Value;
+        }
+
+        public async Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(GoogleLoginUserRequestDto requestDto)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings
+            {
+                Audience = [options.ClientId]
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(requestDto.IdToken, settings);
+            return payload;
+        }
+
+    }
+}
