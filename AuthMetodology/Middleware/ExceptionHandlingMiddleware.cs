eec309fd7284555dc919eef0f83e9e4b9d3f690a
BelugaChan <tuchnyy@list.ru>
2025-03-24 20:40:31 +0300

    Added new exception handling options

--- AuthMetodology/Middleware/ExceptionHandlingMiddleware.cs
+++ AuthMetodology/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using AuthMetodology.Application.Exceptions;
+using Google.Apis.Auth;
 using System.Net;
 
 namespace AuthMetodology.API.Middleware
@@ -28,6 +29,9 @@ namespace AuthMetodology.API.Middleware
                 ExistMailException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Пользователь с такой почтой уже существует"),
                 IncorrectPasswordException _ => new ExceptionResponse(HttpStatusCode.Unauthorized, "Неверный пароль"),
                 IncorrectMailException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Пользователь с такой почтой не найден в системе"),
+                UserNotFoundException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Пользователь с таким id не существует"),
+                InvalidRefreshTokenException _ => new ExceptionResponse(HttpStatusCode.Unauthorized, "Invalid refresh token"),
+                InvalidJwtException _ => new ExceptionResponse(HttpStatusCode.BadRequest, "Invalid token (called from GoogleLogin)"),
                 _ => new ExceptionResponse(HttpStatusCode.InternalServerError, "Internal server error. Please retry later")
             };
 
