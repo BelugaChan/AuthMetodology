cc8dadc4bd2bb9923930a73ccb5de1f182941484
BelugaChan <tuchnyy@list.ru>
2025-03-24 20:48:49 +0300

    Added new interface for verifying google token

---
+++ AuthMetodology.Application/Interfaces/IGoogleService.cs
@@ -0,0 +1,11 @@
+﻿using AuthMetodology.Application.DTO;
+using AuthMetodology.Logic.Models;
+using Google.Apis.Auth;
+
+namespace AuthMetodology.Application.Interfaces
+{
+    public interface IGoogleService
+    {
+        Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(GoogleLoginUserRequestDto requestDto);
+    }
+}
