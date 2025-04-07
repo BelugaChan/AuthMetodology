14db78704ba2d89a0a629ec25cfbf1c963988f0b
BelugaChan <tuchnyy@list.ru>
2025-04-07 10:30:30 +0300

    Added new exception types

---
+++ AuthMetodology.Application/Exceptions/IncorrectGoogleCredentialsException.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthMetodology.Application.Exceptions
+{
+    public class IncorrectGoogleCredentialsException : Exception
+    {
+        public IncorrectGoogleCredentialsException()
+        {
+        }
+
+        public IncorrectGoogleCredentialsException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
