86df6d1a8f86a020bb68de235a1c3d55bbd3f7c0
BelugaChan <tuchnyy@list.ru>
2025-04-07 10:31:42 +0300

    Added new interface for two fa buisness logic

---
+++ AuthMetodology.Application/Interfaces/ITwoFaService.cs
@@ -0,0 +1,22 @@
+﻿using AuthMetodology.Application.DTO.v1;
+using AuthMetodology.Logic.Models.v1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthMetodology.Application.Interfaces
+{
+    public interface ITwoFaService
+    {
+        Task EnableTwoFaStatusAsync(Guid id);
+        Task DisableTwoFaStatusAsync(Guid id);
+
+        Task<AuthResponseDtoV1> VerifyTwoFaCodeAsync(TwoFaRequestDto requestDto);
+
+        Task SendTwoFaAsync(SendTwoFaRequestDto requestDto);
+
+        Task SendTwoFaAsync(ReSendTwoFaRequestDto requestDto);
+    }
+}
