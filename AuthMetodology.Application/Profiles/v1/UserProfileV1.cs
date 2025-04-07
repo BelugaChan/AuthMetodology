51a4285ce9a159c1dcc003de6e21e0da5f77fbee
BelugaChan <tuchnyy@list.ru>
2025-04-07 10:32:10 +0300

    New class path

---
+++ AuthMetodology.Application/Profiles/v1/UserProfileV1.cs
@@ -0,0 +1,15 @@
+﻿using AuthMetodology.Logic.Entities.v1;
+using AuthMetodology.Logic.Models.v1;
+using AutoMapper;
+
+namespace AuthMetodology.Application.Profiles.v1
+{
+    public class UserProfileV1 : Profile
+    {
+        public UserProfileV1()
+        {
+            CreateMap<UserEntityV1, UserV1>();
+            CreateMap<UserV1, UserEntityV1>();
+        }
+    }
+}
