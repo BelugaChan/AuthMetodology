3b82edcc63f2860476350c0d100b090bbccb8575
BelugaChan <tuchnyy@list.ru>
2025-04-07 10:24:48 +0300

    Minor fixes: changed class path

---
+++ AuthMetodology/Controllers/v1/TestAuthController.cs
@@ -0,0 +1,21 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthMetodology.API.Controllers.v1
+{
+    [ApiVersion(1)]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/testAuth")]
+    public class TestAuthController : ControllerBase
+    {
+        [Authorize]
+        [MapToApiVersion(1)]
+        [HttpGet]
+        public IActionResult GetTestData()
+        {
+            var data = new {Name = "Test", Surname = "Testovich" };
+            return Ok(data);
+        }
+    }
+}
