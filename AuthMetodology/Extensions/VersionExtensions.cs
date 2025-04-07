ec102eb46c9feee73ee73e206117f44353fd31d3
BelugaChan <tuchnyy@list.ru>
2025-04-07 10:26:42 +0300

    Minor fixes

---
+++ AuthMetodology/Extensions/VersionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+
+namespace AuthMetodology.API.Extensions
+{
+    public static class VersionExtensions
+    {
+        public static void AddVersioning(this IServiceCollection services) 
+        {
+            services.AddApiVersioning(options =>
+            {
+                //Версия API по умолчанию
+                options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1);
+
+                //Cпециальные HTTP-заголовки, в которых перечислены актуальные и устаревшие версии API
+                options.ReportApiVersions = true;
+
+                //Используем версию API по умолчанию, если клиент явно не указал нужную ему
+                options.AssumeDefaultVersionWhenUnspecified = true;
+
+                //Определяем, что будем ожидать нужную версию API в самой строке запроса или в URL-сегменте
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new UrlSegmentApiVersionReader(),//site.com/v2/getdata
+                    new QueryStringApiVersionReader("version")); //site.com/getdata?version=2
+            })
+                .AddMvc()
+                //Данный метод исправляет конечные маршруты и подставляет нужную версию API через параметр в маршруте.
+                .AddApiExplorer(options =>
+                {
+                    options.GroupNameFormat = "'v'V";
+                    options.SubstituteApiVersionInUrl = true;
+                });
+        }
+    }
+}
