2f60e3038a41d0baef5d3e84d165cd14084e13c5
BelugaChan <tuchnyy@list.ru>
2025-04-09 14:51:45 +0300

    New migrations

---
+++ AuthMetodology.Persistence/Migrations/20250407155900_GoogleAndTwoFa.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthMetodology.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class GoogleAndTwoFa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "idGoogle",
+                schema: "authSchema",
+                table: "auth",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is2FAEnabled",
+                schema: "authSchema",
+                table: "auth",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "idGoogle",
+                schema: "authSchema",
+                table: "auth");
+
+            migrationBuilder.DropColumn(
+                name: "is2FAEnabled",
+                schema: "authSchema",
+                table: "auth");
+        }
+    }
+}
