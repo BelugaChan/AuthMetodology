f38f093e5875b6486ed73311681e314e341e12db
BelugaChan <tuchnyy@list.ru>
2025-04-09 14:53:26 +0300

    Added new two fa DTOs
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ AuthMetodology.Application/DTO/v1/ReSendTwoFaRequestDtoV1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthMetodology.Application.DTO.v1
+{
+    public class ReSendTwoFaRequestDtoV1
+    {
+        public required string Email { get; set; }
+    }
+}
---
+++ AuthMetodology.Application/DTO/v1/TwoFaRequestDtoV1.cs
@@ -0,0 +1,23 @@
+﻿using AuthMetodology.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthMetodology.Application.DTO.v1
+{
+    public class TwoFaRequestDtoV1
+    {
+        public required Guid Id { get; set; }
+
+        public required string Code { get; set; }
+
+        public static TwoFaRequestDtoV1 Create(Guid id, string code)
+            => new TwoFaRequestDtoV1()
+            {
+                Id = id,
+                Code = code
+            };
+    }
+}
