59ba0780c6cdbc816164382ecb4e109af00167cd
BelugaChan <tuchnyy@list.ru>
2025-04-16 17:07:31 +0300

    Added user role enum and updated user model with user role and reset token settings
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- AuthMetodology.Logic/Entities/v1/UserEntityV1.cs
+++ AuthMetodology.Logic/Entities/v1/UserEntityV1.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AuthMetodology.Logic.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AuthMetodology.Logic.Entities.v1
@@ -28,7 +29,16 @@ namespace AuthMetodology.Logic.Entities.v1
         [Column("is2FAEnabled")]
         public bool Is2FaEnabled { get; set; } = false;
 
-        public static UserEntityV1 Create(Guid id,string passwordHash, string email, string refreshToken, DateTime refreshTokenExpiry, string idGoogle, bool is2FaEnabled)
+        [Column("userRole")]
+        public UserRole UserRole { get; set; } = UserRole.User;
+
+        [Column("resetPasswordToken")]
+        public string ResetPasswordToken { get; set; } = string.Empty;
+
+        [Column("resetPasswordTokenExpiry")]
+        public DateTime ResetPasswordTokenExpiry { get; set; }
+
+        public static UserEntityV1 Create(Guid id,string passwordHash, string email, string refreshToken, DateTime refreshTokenExpiry, string idGoogle, bool is2FaEnabled, string resetPasswordToken, DateTime resetPasswordTokenExpiry)
         {
             return new UserEntityV1() { Email = email, 
                                         Id = id, 
@@ -36,7 +46,9 @@ namespace AuthMetodology.Logic.Entities.v1
                                         RefreshToken = refreshToken,
                                         RefreshTokenExpiry = refreshTokenExpiry,
                                         IdGoogle = idGoogle,
-                                        Is2FaEnabled = is2FaEnabled};
+                                        Is2FaEnabled = is2FaEnabled,
+                                        ResetPasswordToken = resetPasswordToken,
+                                        ResetPasswordTokenExpiry = resetPasswordTokenExpiry};
         }
     }
 }
