d24789530641987f14c85534c6e7bb5c8f28fb23
BelugaChan <tuchnyy@list.ru>
2025-04-16 16:44:30 +0300

    General changes: added OpenTelemetry support and registered new services in DI container
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- AuthMetodology/Program.cs
+++ AuthMetodology/Program.cs
@@ -16,8 +16,10 @@ using AuthMetodology.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using Serilog;
-using Serilog.Sinks.Grafana.Loki;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using RabbitMqModel.Models;
+using RabbitMqPublisher.Interface;
 using StackExchange.Redis;
 namespace AuthMetodology.API
 {
@@ -26,38 +28,55 @@ namespace AuthMetodology.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+           
+
             //add API versioning
             builder.Services.AddVersioning();
 
-            var logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Configuration)
-                .CreateLogger();
+            //add logging
+            //var logger = new LoggerConfiguration()
+            //    .ReadFrom.Configuration(builder.Configuration)
+            //    .CreateLogger();
 
-            builder.Logging.ClearProviders();
-            builder.Logging.AddSerilog(logger);
+            //builder.Logging.ClearProviders();
+            //builder.Logging.AddSerilog(logger);
+
+            //add openTelemetry
+            var openTelemetryBuilder = builder.Services.AddOpenTelemetry();
+
+            openTelemetryBuilder.ConfigureResource(source =>
+                source.AddService(builder.Environment.ApplicationName));
+
+            openTelemetryBuilder.WithMetrics(metrics =>
+                metrics
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter()
+                .AddPrometheusExporter(o =>
+                {
+                    o.DisableTotalNameSuffixForCounters = false;
+                    o.ScrapeResponseCacheDurationMilliseconds = 30000;
+                })
+                );
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API - V1", Version = "v1.0" });
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API - V1", Version = "v1.0" });
 
-                var basePath = AppContext.BaseDirectory;
-                var xmlPath = Path.Combine(basePath, "AuthMetodology.xml");
-                c.IncludeXmlComments(xmlPath);
-            }
-                
-
-                
+                    var basePath = AppContext.BaseDirectory;
+                    var xmlPath = Path.Combine(basePath, "AuthMetodology.xml");
+                    c.IncludeXmlComments(xmlPath);
+                }     
             );
 
             builder.Services.Configure<JWTOptions>(builder.Configuration.GetSection(nameof(JWTOptions)));
             builder.Services.Configure<GoogleOptions>(builder.Configuration.GetSection(nameof(GoogleOptions)));
             builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection(nameof(RabbitMqOptions)));
+            builder.Services.Configure<ResetPasswordOptions>(builder.Configuration.GetSection(nameof(ResetPasswordOptions)));
+
             //builder.Services.Configure<EmailOptions>(builder.Configuration.GetSection(nameof(EmailOptions)));
 
             builder.Services.AddApiAuthentication(builder.Services.BuildServiceProvider().GetRequiredService<IOptions<JWTOptions>>());
@@ -67,12 +86,15 @@ namespace AuthMetodology.API
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
             builder.Services.AddScoped<ICookieCreator, CookieTokenCreator>();
-            builder.Services.AddScoped<IRabbitMqService, RabbitMqService>();
             builder.Services.AddScoped<IRedisService, RedisService>();
             builder.Services.AddScoped<ITwoFaProvider, TwoFaProvider>();
             builder.Services.AddScoped<ITwoFaService, TwoFaService>();
             builder.Services.AddScoped<IGoogleService, GoogleService>();
             builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddSingleton<IRabbitMqPublisherBase<RabbitMqLogPublish>, LogQueueService>();
+            builder.Services.AddSingleton<IRabbitMqPublisherBase<RabbitMqResetPasswordPublish>, ResetPasswordQueueService>();
+            builder.Services.AddSingleton<IRabbitMqPublisherBase<RabbitMqTwoFaPublish>, TwoFaQueueService>();
 
             builder.Services.AddAutoMapper(typeof(UserProfileV1).Assembly);
 
@@ -91,6 +113,8 @@ namespace AuthMetodology.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.MapPrometheusScrapingEndpoint();
 
             //app.UseHttpsRedirection();
 
