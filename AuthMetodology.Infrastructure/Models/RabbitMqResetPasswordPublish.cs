2d90899bd98913f596e79427019423ada1c739ba
BelugaChan <tuchnyy@list.ru>
2025-04-16 17:02:59 +0300

    Created new rabbit mq event models
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ AuthMetodology.Infrastructure/Models/RabbitMqResetPasswordPublish.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthMetodology.Infrastructure.Models
+{
+    public class RabbitMqResetPasswordPublish
+    {
+        public required string Link { get; set; }
+
+        public required string Email { get; set; }
+
+        public static RabbitMqResetPasswordPublish Create(string link, string email)
+            => new RabbitMqResetPasswordPublish() 
+            { 
+                Link = link,
+                Email = email
+            };
+    }
+}
