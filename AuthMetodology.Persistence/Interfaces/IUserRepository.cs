a210de3250c433199ed54207dad1dbbc499f1b11
BelugaChan <tuchnyy@list.ru>
2025-04-16 17:12:24 +0300

    Added cancellation tokens support
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- AuthMetodology.Persistence/Interfaces/IUserRepository.cs
+++ AuthMetodology.Persistence/Interfaces/IUserRepository.cs
@@ -4,12 +4,14 @@ namespace AuthMetodology.Persistence.Interfaces
 {
     public interface IUserRepository
     {
-        Task AddAsync(UserEntityV1 user);
+        Task AddAsync(UserEntityV1 user, CancellationToken cancellationToken = default);
 
-        Task<UserEntityV1?> GetByEmailAsync(string email);
+        Task<UserEntityV1?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
 
-        Task<UserEntityV1?> GetByIdAsync(Guid id);
+        Task<UserEntityV1?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 
-        Task<bool> UpdateUserAsync(Guid id, Action<UserEntityV1> updateUser);
+        Task<UserEntityV1?> GetByResetPasswordTokenAsync(string resetPasswordToken, CancellationToken cancellationToken = default);
+
+        Task<bool> UpdateUserAsync(Guid id, Action<UserEntityV1> updateUser, CancellationToken cancellationToken = default);
     }
 }
