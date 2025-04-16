d447951af013d6e94f89d7a55cd0e3e22b4dbf8f
BelugaChan <tuchnyy@list.ru>
2025-04-16 17:13:18 +0300

    Some canellation token support was added
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- AuthMetodology.Persistence/Repositories/UserRepository.cs
+++ AuthMetodology.Persistence/Repositories/UserRepository.cs
@@ -10,37 +10,45 @@ namespace AuthMetodology.Persistence.Repositories
         private readonly UserDBContext context;
         public UserRepository(UserDBContext context) => this.context = context;
 
-        public async Task AddAsync(UserEntityV1 userEntity)
+        public async Task AddAsync(UserEntityV1 userEntity, CancellationToken cancellationToken = default)
         {
-            await context.Users.AddAsync(userEntity);
-            await context.SaveChangesAsync();
+            await context.Users.AddAsync(userEntity, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
         }
 
-        public async Task<UserEntityV1?> GetByEmailAsync(string email)
+        public async Task<UserEntityV1?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
         {
-            var userEntity = await context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email == email);
+            var userEntity = await context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email == email, cancellationToken);
             if (userEntity is null)
                 return null;
             return userEntity;
         }
 
-        public async Task<UserEntityV1?> GetByIdAsync(Guid id)
+        public async Task<UserEntityV1?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
         {
             //mapper.Map<UserV1>(userEntity)
-            var userEntity = await context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Id == id);
+            var userEntity = await context.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
             if (userEntity is null)
                 return null;
             return userEntity;
         }
 
-        public async Task<bool> UpdateUserAsync(Guid id, Action<UserEntityV1> updateUser)
+        public async Task<UserEntityV1?> GetByResetPasswordTokenAsync(string resetPasswordToken, CancellationToken cancellationToken = default)
         {
-            var userEntity = await context.Users.FindAsync(id);
+            var userEntity = await context.Users.FirstOrDefaultAsync(user => user.ResetPasswordToken == resetPasswordToken, cancellationToken);
+            if (userEntity is null)
+                return null;
+            return userEntity;
+        }
+
+        public async Task<bool> UpdateUserAsync(Guid id, Action<UserEntityV1> updateUser, CancellationToken cancellationToken)
+        {
+            var userEntity = await context.Users.FindAsync(id, cancellationToken);
             if (userEntity is null)
                 return false;
             
             updateUser(userEntity);
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(cancellationToken);
             return true;
         }
     }
