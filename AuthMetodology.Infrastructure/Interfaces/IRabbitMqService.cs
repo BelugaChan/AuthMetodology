d528ff2a78ccf84f90f37d23c71651a697273553
BelugaChan <tuchnyy@list.ru>
2025-04-16 17:16:39 +0300

    Some changes

--- AuthMetodology.Infrastructure/Interfaces/IRabbitMqService.cs
+++
@@ -1,7 +0,0 @@
-namespace AuthMetodology.Infrastructure.Interfaces;
-
-public interface IRabbitMqService
-{
-    void SendMessage(object obj);
-    void SendMessage(string message);
-}
--- AuthMetodology.Infrastructure/Models/RabbitMqOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthMetodology.Infrastructure.Models;
-
-public class RabbitMqOptions
-{
-    public string Host { get; set; } = string.Empty;
-
-    public string Queue { get; set; } = string.Empty;
-}
