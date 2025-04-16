3c40630af8bc678a3ae21abe3975f8a36d50ae0f
BelugaChan <tuchnyy@list.ru>
2025-04-16 16:46:11 +0300

    New DTO which use in reset password cases
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ AuthMetodology.Application/DTO/v1/ForgotPasswordRequestDtoV1.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthMetodology.Application.DTO.v1
+{
+    public class ForgotPasswordRequestDtoV1
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [Length(3, 30, ErrorMessage = "Must be between 5 and 30 characters")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ AuthMetodology.Application/DTO/v1/ResetPasswordRequestDtoV1.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthMetodology.Application.DTO.v1
+{
+    public class ResetPasswordRequestDtoV1
+    {
+        public required string Token { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Пароль должен быть от 8 до 20 символов")]
+        [RegularExpression(@"^(?=[^А-Яа-я]*$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+        ErrorMessage = "Пароль должен содержать цифры, спецсимволы, латинские буквы в верхнем и нижнем регистре и не должен содержать кириллицу")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirmation is required")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Пароль должен быть от 8 до 20 символов")]
+        [RegularExpression(@"^(?=[^А-Яа-я]*$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+        ErrorMessage = "Пароль должен содержать цифры, спецсимволы, латинские буквы в верхнем и нижнем регистре и не должен содержать кириллицу")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
