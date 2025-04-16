0537a0bc9ed8d37ad0ca441f3879cafc2e700ef5
BelugaChan <tuchnyy@list.ru>
2025-04-16 16:47:02 +0300

    Added new custom exception models
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ AuthMetodology.Application/Exceptions/ExpiredResetPasswordTokenException.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthMetodology.Application.Exceptions
+{
+    public class ExpiredResetPasswordTokenException : Exception
+    {
+        public ExpiredResetPasswordTokenException()
+        {
+            
+        }
+
+        public ExpiredResetPasswordTokenException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
---
+++ AuthMetodology.Application/Exceptions/InvalidTwoFaStatusException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthMetodology.Application.Exceptions
+{
+    public class InvalidTwoFaStatusException : Exception
+    {
+        public InvalidTwoFaStatusException() { }
+
+        public InvalidTwoFaStatusException(string message)
+            :base(message)
+        {
+            
+        }
+    }
+}
