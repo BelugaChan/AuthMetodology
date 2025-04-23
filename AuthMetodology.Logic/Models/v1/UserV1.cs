d48348b1d526bcd4d98642a383983d9b1a70799c
BelugaChan <tuchnyy@list.ru>
2025-04-23 13:14:04 +0300

    Update param names
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

--- AuthMetodology.Logic/Models/v1/UserV1.cs
+++ AuthMetodology.Logic/Models/v1/UserV1.cs
@@ -1,24 +1,9 @@
 ﻿using AuthMetodology.Logic.Enums;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AuthMetodology.Logic.Models.v1
 {
     public class UserV1
     {
-        private UserV1(Guid id, string passwordHash, string email, string refreshToken, DateTime refreshTokenExpiry, string idGoogle, bool is2FaEnabled, bool isEmailConfirmed, string resetPasswordToken, DateTime resetTokenPasswordExpire)
-        {
-            Id = id;
-            PasswordHash = passwordHash;
-            Email = email;
-            RefreshToken = refreshToken;
-            RefreshTokenExpiry = refreshTokenExpiry;
-            IdGoogle = idGoogle;
-            Is2FaEnabled = is2FaEnabled;
-            IsEmailConfirmed = isEmailConfirmed;
-            ResetPasswordToken = resetPasswordToken;
-            ResetPasswordTokenExpiry = resetTokenPasswordExpire;
-        }
-
         public Guid Id { get; set; }
 
         public string PasswordHash { get; private set; }
@@ -40,7 +25,7 @@ namespace AuthMetodology.Logic.Models.v1
         public string ResetPasswordToken { get; set; }
         public DateTime ResetPasswordTokenExpiry { get; set; }
 
-        public static UserV1 Create(Guid id, string passwordHash, string email, string refreshToken, DateTime refreshTokenExpiry, string idGoogle, bool is2FaEnabled, bool isEmailConfirmed, string resetPasswordToken, DateTime resetTokenPasswordExpire) =>
-            new UserV1(id, passwordHash, email, refreshToken, refreshTokenExpiry, idGoogle, is2FaEnabled, isEmailConfirmed, resetPasswordToken, resetTokenPasswordExpire);
+        public static UserV1 Create(Guid id, string passwordHash, string email, string refreshToken, DateTime refreshTokenExpiry, string idGoogle, bool is2FaEnabled, bool isEmailConfirmed, string resetPasswordToken, DateTime resetPasswordTokenExpiry) =>
+            new UserV1() { Id=id,PasswordHash=passwordHash,Email=email,RefreshToken=refreshToken,RefreshTokenExpiry=refreshTokenExpiry,IdGoogle=idGoogle,Is2FaEnabled=is2FaEnabled,IsEmailConfirmed=isEmailConfirmed,ResetPasswordToken=resetPasswordToken,ResetPasswordTokenExpiry= resetPasswordTokenExpiry };
     }
 }
