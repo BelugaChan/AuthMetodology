c3a42b71789e84a24282ef51db10a41876212908
BelugaChan <tuchnyy@list.ru>
2025-04-28 14:01:35 +0300

    Create new service to implement connection with notification service
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ AuthMetodology.Infrastructure/Services/RegisterUserQueueService.cs
@@ -0,0 +1,20 @@
+﻿using AuthMetodology.Infrastructure.Models;
+using Microsoft.Extensions.Options;
+using RabbitMqModel.Models;
+using RabbitMqPublisher.Abstract;
+
+namespace AuthMetodology.Infrastructure.Services
+{
+    public class RegisterUserQueueService : RabbitMqPublisherBase<RabbitMqUserRegisterPublish>
+    {
+        public RegisterUserQueueService(IOptions<RabbitMqOptions> options) : base(options)
+        {
+        }
+        public override string QueueName => "RegisterUserQueue";
+
+        public override async Task SendEventAsync(RabbitMqUserRegisterPublish message, CancellationToken cancellationToken = default)
+        {
+            await SendMessageAsync(message, QueueName, cancellationToken);
+        }
+    }
+}
